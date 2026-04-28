248bba8fa87e8f3716ced51762615a8f2fc64f03
Kanika Patidar <kp0526@srmist.edu.in>
2026-04-28 09:18:33 +0530

    Initial commit

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,29 +1,33 @@
+import java.util.Random;
+
 public class TicTacToe {
 
-    // Initialize the board
-    public static void initializeBoard(char[][] board) {
-        for (int i = 0; i < 3; i++) {          // rows
-            for (int j = 0; j < 3; j++) {      // columns
-                board[i][j] = '-';
-            }
-        }
-    }
+    static char humanSymbol;
+    static char computerSymbol;
+    static String currentPlayer;
 
-    // Print the board
-    public static void printBoard(char[][] board) {
-        System.out.println("Tic-Tac-Toe Board:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println(); // move to next line after each row
+    // Toss to decide who starts
+    public static void toss() {
+        Random rand = new Random();
+        int result = rand.nextInt(2); // 0 or 1
+
+        if (result == 0) {
+            currentPlayer = "Human";
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            currentPlayer = "Computer";
+            humanSymbol = 'O';
+            computerSymbol = 'X';
         }
+
+        // Display results
+        System.out.println("Toss Result: " + currentPlayer + " starts first!");
+        System.out.println("Human Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
     }
 
     public static void main(String[] args) {
-        char[][] board = new char[3][3];  // 2D array
-
-        initializeBoard(board);  // Step 1: initialize with '-'
-        printBoard(board);       // Step 2: display board
+        toss();
     }
 }
